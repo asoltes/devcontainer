198f75f05dcc4174aff6a1b9141ac044ceece0ac
Andrew Soltes <soltesandrew@gmail.com>
2025-12-13 01:37:24 +0800

    multi stage

--- .devcontainer/library-scripts/custom-zsh-shell.sh
+++ .devcontainer/library-scripts/custom-zsh-shell.sh
@@ -5,11 +5,16 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y install --no-install-recommends \
     fontconfig
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/RobotoMono.zip
-mkdir -p ~/.local/share/fonts/
-unzip RobotoMono.zip -d ~/.local/share/fonts/RobotoMono
-fc-cache -f -v
-rm RobotoMono.zip
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/RobotoMono.zip
+# mkdir -p ~/.local/share/fonts/
+# unzip RobotoMono.zip -d ~/.local/share/fonts/RobotoMono
+# fc-cache -f -v
+# rm RobotoMono.zip
+
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
 
 # oh-my-zsh plugins
 zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/home/vscode/.oh-my-zsh/custom}/themes/powerlevel10k'
